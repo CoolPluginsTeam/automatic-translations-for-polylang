b83dc0a91eacbcb46bdbeb5ce63428738a1a9032
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2024-06-28 16:25:48 +0530

    Scrolling animation timing depends on scrolling height.
    1.) Scrolling animation timing improved depends on scrolling container height.

--- assets/build/index.asset.php
+++ assets/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element'), 'version' => 'd989b88e1c5efb1e7dfc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element'), 'version' => '476b6948dafaae32e25f');
