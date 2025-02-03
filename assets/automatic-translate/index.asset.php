93ab6b3abf22caca090a48a59aee818ce7451f7d
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-02-03 13:03:35 +0530

    Minor changes.
    1. Minor changes revert string modal body commented code.

--- assets/automatic-translate/index.asset.php
+++ assets/automatic-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e254d7cdec2bedef5836');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7d13b44732668b2db1aa');
