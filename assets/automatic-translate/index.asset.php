95d31bcb5493c3d771c3ec519943e711709c1e43
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-04-02 15:25:06 +0530

    Added review link in string modal footer section.
    1. Added review link in string modal footer section.

--- assets/automatic-translate/index.asset.php
+++ assets/automatic-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f7edd146a39091243207');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '61ee1d83c609592c9836');
