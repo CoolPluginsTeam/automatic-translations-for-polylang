e090256cea602e1dd313ed8608a31d5e8c42a726
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-04-09 13:02:39 +0530

    Fixed Chrome Local AI not working in gutenberg & elementor inline translation.
    1. Fixed issue chrome Local AI not working in gutenberg & elementor inline translation after update the chrome.

--- assets/elementor-widget-translator/index.asset.php
+++ assets/elementor-widget-translator/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '7cb58b8cee7670723e0d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '284414837939cfa4005c');
--- includes/common/AiTranslator/isTranslatorApiAvailable.ts
+++ includes/common/AiTranslator/isTranslatorApiAvailable.ts
@@ -1,4 +1,4 @@
 // @ts-ignore
-const isTranslatorApiAvailable = (): boolean => Boolean(window.self.translation);
+const isTranslatorApiAvailable = (): boolean => Boolean(window.self.translation || (window.self.ai && window.self.ai.translator));
 
 export default isTranslatorApiAvailable;
