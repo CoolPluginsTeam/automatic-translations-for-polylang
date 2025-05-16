bbced27d256b716cbe79a626c8745d96fae8e8c3
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-05-16 11:14:47 +0530

    Merge branch 'trunk'
    Merge code from trunk.

--- assets/automatic-translate/index.asset.php
+++ assets/automatic-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ce284d3dec464b625f1b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'dda03449e0bebe1c13f4');
