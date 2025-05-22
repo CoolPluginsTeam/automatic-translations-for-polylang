63c20b666a3cbe21bda6897aa5c3b1cd908e7a23
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-05-22 13:18:13 +0530

    Improve Links.
    Improve Links.

--- admin/atfp-dashboard/views/dashboard.php
+++ admin/atfp-dashboard/views/dashboard.php
@@ -8,12 +8,12 @@
                 <p><?php echo esc_html__('Translate WordPress Pages & Posts instantly with Polylang Addon. One-click, thousands of strings - no extra cost!', $text_domain); ?></p>
                 <div class="atfp-dashboard-btns-row">
                     <a href="<?php echo esc_url(admin_url('edit.php?post_type=page')); ?>" target="_blank" class="atfp-dashboard-btn primary"><?php echo esc_html__('Translate Pages', $text_domain); ?></a>
-                    <a href="<?php echo esc_url(admin_url('admin.php?page=atfp-supported-blocks')); ?>" target="_blank" class="atfp-dashboard-btn"><?php echo esc_html__('Supported Blocks', $text_domain); ?></a>
+                    <a href="<?php echo esc_url(admin_url('admin.php?page=polylang-atfp-dashboard&tab=support-blocks')); ?>" class="atfp-dashboard-btn"><?php echo esc_html__('Supported Blocks', $text_domain); ?></a>
                 </div>
                 <a class="atfp-dashboard-docs" href="<?php echo esc_url('https://docs.coolplugins.net/docs/ai-translation-for-polylang/'); ?>" target="_blank"><img src="<?php echo esc_url(ATFP_URL . 'admin/atfp-dashboard/images/document.svg'); ?>" alt="document"> <?php echo esc_html__('Read Plugin Docs', $text_domain); ?></a>
             </div>
             <div class="atfp-dashboard-welcome-video">
-                <a href="https://locoaddon.com/docs/translate-plugin-theme-via-yandex-translate/?utm_source=atlt_plugin&utm_medium=inside&utm_campaign=docs&utm_content=dashboard_video" target="_blank" class="atfp-dashboard-video-link">
+                <a href="https://coolplugins.net/product/automatic-translations-for-polylang/" target="_blank" class="atfp-dashboard-video-link">
                     <img decoding="async" src="<?php echo ATFP_URL . 'admin/atfp-dashboard/images/video.svg'; ?>" class="play-icon" alt="play-icon">
                     <picture>
                         <source srcset="<?php echo ATFP_URL . 'admin/atfp-dashboard/images/polylang-addon-video.png'; ?>" type="image/webp">
