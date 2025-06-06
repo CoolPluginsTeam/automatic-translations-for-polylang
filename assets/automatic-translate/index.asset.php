a33871eac5b53c5045df2c64d47013fe50eeb5e4
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-06-06 16:03:30 +0530

    Improve chrome local AI error message & Added new language support.
    1. Improve chrome local AI error message.

--- assets/automatic-translate/index.asset.php
+++ assets/automatic-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '74cd938a425d18780034');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd7326b5aff1133975045');
