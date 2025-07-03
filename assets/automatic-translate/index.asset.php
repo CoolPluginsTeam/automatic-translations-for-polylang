182efb9134b39e39b6b2726e5855061f3d3f2e85
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-07-03 12:22:04 +0530

    Added UTM links in modal docs button.
    1. Added UTM links in modal docs button.

--- assets/automatic-translate/index.asset.php
+++ assets/automatic-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '55e0acc04f95747a05e1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a713b1addf4c06910e9c');
