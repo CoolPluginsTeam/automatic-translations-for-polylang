41a4cdae9a140e2fa61b48c6385102f3ed2896f6
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-10-15 16:39:36 +0530

    minor update build files.
    minor update build files.

--- assets/elementor-widget-translator/index.asset.php
+++ assets/elementor-widget-translator/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => 'aeb99e7ed8a2869d9af5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '1675bcaa69c58d692221');
