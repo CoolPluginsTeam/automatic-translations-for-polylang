adc1723f5ac7158ed0344c79db6b6c1ac14fb66a
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2025-12-03 15:11:09 +0530

    Fixed sprintf undefined function error with wordpress 6.9.
    Fixed sprintf undefined function error with wordpress 6.9.

--- assets/automatic-translate/index.asset.php
+++ assets/automatic-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'de2ff4e43886075cb42b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '8d9f51c9854327a0afa8');
