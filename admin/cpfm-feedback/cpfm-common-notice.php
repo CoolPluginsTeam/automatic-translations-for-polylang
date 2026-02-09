45e201d9fb8dba74d2cdf7dc35dd2d4158468e46
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-02-09 16:35:39 +0530

    minor.
    minor.

--- admin/cpfm-feedback/cpfm-common-notice.php
+++ admin/cpfm-feedback/cpfm-common-notice.php
@@ -207,7 +207,7 @@ class CPFM_Feedback_Notice {
             $output .= '<p>' . esc_html__('Opt in to receive email updates about security improvements, new features, helpful tutorials, and occasional special offers. We\'ll collect:', 'automatic-translations-for-polylang') . '</p>';
             $output .= '<ul>';
             $output .= '<li>' . esc_html__('Your website home URL and WordPress admin email.', 'automatic-translations-for-polylang') . '</li>';
-            $output .= '<li>' . esc_html__('To check plugin compatibility, we will collect the following: list of active plugins and themes, server type, MySQL version, WordPress version, memory limit, site language and database prefix', 'automatic-translations-for-polylang');
+            $output .= '<li>' . esc_html__('To check plugin compatibility, we will collect the following: list of active plugins and themes, server type, MySQL version, WordPress version, memory limit, site language and database prefix ', 'automatic-translations-for-polylang');
             $output .= '<a href="' . esc_url('https://my.coolplugins.net/terms/usage-tracking/') . '" target="_blank">' . esc_html__('Click Here', 'automatic-translations-for-polylang') . '.</a> ';
             $output .= '</li>';
             $output .= '</ul>';
