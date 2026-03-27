55e8464c9009629180fb3e73859631f3b92420e9
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-03-27 14:34:43 +0530

    Improve Plugin action links & content.
    1. Improve plugin action Buy Pro style and content.
    2. Added Bulk Translation documentation link after plugin author name.

--- automatic-translation-for-polylang.php
+++ automatic-translation-for-polylang.php
@@ -77,6 +77,7 @@ if ( ! class_exists( 'AutoPoly' ) ) {
 			// Initialize feedback notice.
 			$this->init_feedback_notice();
 			add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'atfp_plugin_action_links' ) );
+			add_filter( 'plugin_row_meta', array( $this, 'atfp_plugin_row_links' ), 10, 2 );
 
 			// nonce verification is not required here because we are not using the nonce here.
 			// phpcs:ignore WordPress.Security.NonceVerification.Recommended
@@ -97,8 +98,21 @@ if ( ! class_exists( 'AutoPoly' ) ) {
 				$atfp_utm_parameters=ATFP_Helper::utm_source_text();
 			}
 
-			$links[] = '<a href="https://coolplugins.net/product/autopoly-ai-translation-for-polylang/?'.sanitize_text_field($atfp_utm_parameters).'&utm_medium=inside&utm_campaign=get_pro&utm_content=plugin_list" target="_blank">' . __( 'Buy Pro', 'automatic-translations-for-polylang' ) . '</a>';
+			$links[] = '<a href="https://coolplugins.net/product/autopoly-ai-translation-for-polylang/?'.sanitize_text_field($atfp_utm_parameters).'&utm_medium=inside&utm_campaign=get_pro&utm_content=plugin_list" target="_blank" style="font-weight:bold; color:#852636;">' . __( 'Get Pro', 'automatic-translations-for-polylang' ) . '</a>';
 			return $links;
+		}
+
+		function atfp_plugin_row_links( $plugin_meta, $plugin_file ) {
+			if ( $plugin_file === plugin_basename( __FILE__ ) ) {
+				$atfp_utm_parameters='utm_source=atfp_plugin';
+	
+				if(class_exists('ATFP_Helper')){
+					$atfp_utm_parameters=ATFP_Helper::utm_source_text();
+				}
+				
+				$plugin_meta[] = '<a href="'.esc_url('https://docs.coolplugins.net/doc/ai-translation-polylang-bulk-translation/?'.sanitize_text_field($atfp_utm_parameters).'&utm_medium=inside&utm_campaign=docs&utm_content=plugin_list').'" target="_blank" style="font-weight:bold;">' . __( 'Bulk Translation', 'automatic-translations-for-polylang' ) . '</a>';
+			}
+			return $plugin_meta;
 		}
 
 		public function atfp_plugin_redirection($plugin) {
