2e52df4081a68c3451560235b65bc90bd56fbf16
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-04-06 16:22:12 +0530

    Remove beta.
    Remove beta.

--- assets/automatic-translate/index.asset.php
+++ assets/automatic-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b16c42b67b8a662d7ba4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd502032b6b90f0ac133c');
