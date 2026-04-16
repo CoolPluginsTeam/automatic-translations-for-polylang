2cdd74e7e17c8ca4ceaa86f7b6b87a6ae290a2bc
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-04-16 13:27:37 +0530

    Fixed yandex translation isssue.
    Fixed yandex translation isssue.

--- assets/automatic-translate/index.asset.php
+++ assets/automatic-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '738bde0a0cc87e594a28');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e606db78229e8d8ab340');
