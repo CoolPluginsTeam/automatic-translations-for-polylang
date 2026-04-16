691b07e3843c015b03625ed3573d56d5c8da37a1
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-04-16 11:45:45 +0530

    Improve chrome local translation AI modal error state.
    Improve chrome local translation AI modal error state.

--- assets/automatic-translate/index.asset.php
+++ assets/automatic-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd502032b6b90f0ac133c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '738bde0a0cc87e594a28');
