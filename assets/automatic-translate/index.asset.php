2600ba11f0e11aaed5a1744cfe8afb4d5419da09
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-04-17 17:57:11 +0530

    Minor.
    minor.

--- assets/automatic-translate/index.asset.php
+++ assets/automatic-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '74ee87022df03b7453e8');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'dadbdfca12e1bbbee507');
