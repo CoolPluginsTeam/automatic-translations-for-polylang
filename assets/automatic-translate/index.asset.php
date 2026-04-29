3fe6e4e494c4d9dcf3befc9f7fb6ba7da7adf767
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-04-29 15:30:50 +0530

    Improve content filtration and fixed URL formatting issue after translation.
    Improve content filtration and fixed URL formatting issue after translation.

--- assets/automatic-translate/index.asset.php
+++ assets/automatic-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5e4e2b5aa166819d542e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ad86cdd4e59633886de1');
