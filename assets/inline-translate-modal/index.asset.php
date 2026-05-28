950cb58b3a823b830dc0eccbd81929573795e0bd
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-05-28 14:30:55 +0530

    Preserve inline translation text white space during translation.
    Preserve inline translation text white space during translation.

--- assets/inline-translate-modal/index.asset.php
+++ assets/inline-translate-modal/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4ec38f25147200f01a06');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f1894f34ba13ff4a4abc');
