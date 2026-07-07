331c6cc9d3ca6312016221b57e2c19c4c42752c4
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-07-07 15:50:55 +0530

    Fixed empty block added after translation in gutenberg.
    Fixed empty block added after translation in gutenberg.
    Fixed block translation not working when divi theme activate and divi placeholder block added in gutenberg page.

--- assets/automatic-translate/index.asset.php
+++ assets/automatic-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'de3067a9eac9b35f462f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '25bc0bfa954ec1c4abc8');
