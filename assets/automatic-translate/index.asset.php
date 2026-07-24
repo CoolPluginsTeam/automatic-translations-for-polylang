0cf8156230ae3c519b1c7556027b1a0f2de880eb
aniketcoolplugins007 <aniket.coolplugins@gmail.com>
2026-07-24 12:24:20 +0530

    Fixed elementor repeater control URL link translation issue.
    Fixed elementor repeater control URL link translation issue.

--- assets/automatic-translate/index.asset.php
+++ assets/automatic-translate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0ba0777359b27405cd26');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '13ff06de296fbb852c5e');
